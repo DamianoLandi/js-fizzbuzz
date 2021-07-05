39fd52c025f3687f92beb04887453794133ace32
DamianoLandi <damianolandi01@gmail.com>
2021-07-05 17:03:00 +0200

    JS Fix

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@ var numbList = document.getElementById("numbList");
 for (var i = 1; i < 101; i++){
     if((i % 3 == 0) && (i % 5 == 0)){
         print += "<li>FizzBuzz</li>";
-    }else if ((i % 3 == 0) && (i % 5 != 0)){
+    }else if (i % 3 == 0){
         print += "<li>Fizz</li>"
-    }else if ((i % 3 != 0) && (i % 5 == 0)){
+    }else if (i % 5 == 0){
         print += "<li>Buzz</li>"
     }else{
         print += ("<li>" + i + "</li>");
