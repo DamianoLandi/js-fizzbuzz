605e96d31216069108aeaa1c78db83668618fae4
DamianoLandi <damianolandi01@gmail.com>
2021-07-05 16:15:20 +0200

    Logic

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ var print = "";
 
 var numbList = document.getElementById("numbList");
 
+//Logica
+
+for (var i = 1; i < 101; i++){
+    if((i % 3 == 0) && (i % 5 == 0)){
+        print += "<li>FizzBuzz</li>";
+    }else if ((i % 3 == 0) && (i % 5 != 0)){
+        print += "<li>Fizz</li>"
+    }else if ((i % 3 != 0) && (i % 5 == 0)){
+        print += "<li>Buzz</li>"
+    }else{
+        print += ("<li>" + i + "</li>");
+    }
+}
